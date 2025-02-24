f99182a2fe780d184557b57f79bfea5add5e7a69
Viottery <xc1330948336@163.com>
2025-02-24 23:28:38 +0800

    message reactor

--- QQChatBot/QQChatBot/plugins/echo2test/__init__.py
+++ QQChatBot/QQChatBot/plugins/echo2test/__init__.py
@@ -1,8 +1,16 @@
 from nonebot import on_message
 from nonebot.adapters.onebot.v11 import Bot, Event
+from .llm_module import LLMInterface
+from nonebot.rule import Rule, to_me
+
+
+async def check_tome(event: Event) -> bool:
+    return True
+
+rule = Rule(check_tome, to_me())
 
 # 创建一个匹配所有消息事件的
-echo2 = on_message(priority=10, block=False)
+echo2 = on_message(priority=10, block=False, rule=rule)
 
 
 @echo2.handle()
