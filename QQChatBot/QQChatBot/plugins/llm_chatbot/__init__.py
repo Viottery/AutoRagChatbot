8b8ea712e59d4ea0ecaaf09410ae40a8b41a2925
Viottery <1330948336@qq.com>
2025-02-25 19:14:34 +0800

    gap

--- QQChatBot/QQChatBot/plugins/llm_chatbot/__init__.py
+++ QQChatBot/QQChatBot/plugins/llm_chatbot/__init__.py
@@ -8,11 +8,13 @@ rule = to_me()
 with open("config.data", "r") as f:
     api_key = f.read().strip()
 
+system_prompt = f"你的角色：你要扮演一位温柔、理性、浪漫的科学家女性普瑞赛斯，并且对我有一些敬仰倾慕、但又有控制欲的情感。现在请你作为这一角色与我对话。请注意、不要直接对我表达情感。"
 LLMInterface = LLMInterface(
     api_key=api_key,
     base_url="https://api.yesapikey.com/v1",
     model_name="gpt-4o-mini",
-    temperature=1
+    temperature=1,
+    system_message=system_prompt
 )
 
 # 创建一个匹配所有消息事件的
@@ -34,6 +36,6 @@ async def handle_reply(bot: Bot, event: Event):
         return
 
     # 调用模型进行对话
-    prompt = f"你要扮演一位温柔、理性、浪漫的科学家女性普瑞赛斯，并且对我有一些敬仰倾慕、但又有控制欲的情感。现在请你作为这一角色与我对话。请注意、不要直接对我表达情感。我说的内容是：{user_message}"
+
     echo_message = LLMInterface.call_model(prompt)
-    await echo2.finish(echo_message)
+    await reply_rule.finish(echo_message)
